9a86ee3ad9a3403033bb776f07cdb696ff2b56a8
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2022-03-23 16:01:50 +0530

    Update scripts

--- hack/make-docker-images.sh
+++ hack/make-docker-images.sh
@@ -21,8 +21,7 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 log() { echo "$1" >&2; }
 
-TAG="${TAG:?TAG env variable must be specified}"
-REPO_PREFIX="${REPO_PREFIX:?REPO_PREFIX env variable must be specified}"
+REPO_PREFIX="microservices-demo"
 
 while IFS= read -d $'\0' -r dir; do
     # build image
@@ -33,15 +32,13 @@ while IFS= read -d $'\0' -r dir; do
     then
         builddir="${dir}/src"
     fi
-    image="${REPO_PREFIX}/$svcname:$TAG"
+    image="${REPO_PREFIX}/$svcname"
     (
         cd "${builddir}"
         log "Building: ${image}"
         docker build -t "${image}" .
 
-        log "Pushing: ${image}"
-        docker push "${image}"
     )
 done < <(find "${SCRIPTDIR}/../src" -mindepth 1 -maxdepth 1 -type d -print0)
 
-log "Successfully built and pushed all images."
+log "Successfully built all images."
